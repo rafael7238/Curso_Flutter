67d4bda5e7f577b249ec25c74995945b8ed0283f
rafael7238 <rafael7238@hotmail.com>
2021-08-03 21:20:30 -0300

    Primeiro Commit

---
+++ Flutter/cattalk/android/app/src/main/kotlin/com/example/cattalk/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cattalk
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Flutter/cattalk/lib/main.dart
@@ -0,0 +1,88 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(
+          title: 'Lucianna futura programadora robótica para medicina'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.share),
+              onPressed: () {
+                setState(() {
+                  _counter++;
+                });
+              })
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+            Text(_counter % 2 == 0 ? "É múltiplo de 2" : "Não é mútiplo de 2"),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  _counter--;
+                });
+              },
+              child: Text(
+                "-1",
+                style: TextStyle(fontSize: 50),
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
